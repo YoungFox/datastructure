aba55b32578f7f3336666d72c08bfee902dbcf55
YANG <yang6233562@gmail.com>
2017-03-14 22:40:05 +0800

    comprehention

--- src/helloworld.c
+++ src/helloworld.c
@@ -2,6 +2,12 @@
 
 #include<stdio.h>
 
-int main(){
-	printf("Hello World!!\n");
+void main(){
+	int a,b;
+	int * pointer1,* pointer2;
+	a=100;b=10;
+	pointer1 = &a;
+	pointer2 = &b;
+	printf("%d,%d\n",a,b);
+	printf("%d,%d\n",&pointer1,&pointer2);
 }
