a146a0931c9b52cc7fd277e5b626033bbe1b6461
YANG <yang6233562@gmail.com>
2017-03-18 12:11:56 +0800

    char

--- src/helloworld.c
+++ src/helloworld.c
@@ -15,15 +15,36 @@ void main(){
 	// printf("%d,%d,%d,%d\n",pointer1,pointer2,pointer3,pointer4);
 
 
-	int x = 100;
-    char str[20] = "c.biancheng.net";
-    printf("%#X, %#X\n", &x, str);
+	// int x = 100;
+    // char str[20] = "c.biancheng.net";
+    // printf("%#X, %#X\n", &x, str);
 
-	int a = 15, b = 99, c = 222;
-    int *p = &a;  //定义指针变量
-    *p = b;  //通过指针变量修改内存上的数据
-    c = *p;  //通过指针变量获取内存上的数据
-    printf("%d, %d, %d, %d,%d\n", a, b, c, *p, p);
+	// int a = 15, b = 99, c = 222;
+    // int *p = &a;  //定义指针变量
+    // *p = b;  //通过指针变量修改内存上的数据
+    // c = *p;  //通过指针变量获取内存上的数据
+    // printf("%d, %d, %d, %d,%d\n", a, b, c, *p, p);
 
-	printf("%d,%s",NULL,NULL);
+	// printf("%d,%s\n",NULL,NULL);
+	
+
+	 /* strlen:  return length of string s */
+   int strlen(char *s)
+	{
+		int n;
+		for (n = 0; *s != '\0'; s++)
+		{	n++;
+		printf("char is:%c\n",*s);
+		}
+		return n; 
+	}
+
+	char char1[3] = {'a','b'};
+	char arr[10];
+	char *p1 = char1;
+	// int l1 = strlen("akjfladjfkj");
+	// int l2 = strlen(arr);
+	int l3 = strlen(p1);
+	printf("%c",char1[5]);
+	// printf("tree lengths:%d,%d,%d",l1,l2,l3);
 }
