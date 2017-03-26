5a33abe2b9f9cb06f954be3b591ee7a58c08ac70
YANG <yang6233562@gmail.com>
2017-03-26 22:29:44 +0800

    queue

--- src/ch3/main3-3.c
+++ src/ch3/main3-3.c
@@ -8,7 +8,7 @@ void visit(QElemType i){
     printf("%d",i);
 }
 
-int main(){
+void main(){
     Status j;
     int i = 0;
     int l;
@@ -37,5 +37,4 @@ int main(){
     }
     ClearQueue(&Q);
     printf("清空队列后，队列是否为空？%u(1:空 0：否)\n",QueueEmpty(Q));
-    return 0;
 }
